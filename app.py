17f7674ae66dbfff1a871610d16241c7dfc9968c
naferene <34173435+naferene@users.noreply.github.com>
2026-02-15 01:34:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import os
+import sys
+sys.path.append(os.path.dirname(__file__))
+
 
 from engine.directional_engine import calculate_directional_score
 from engine.regime_engine import detect_regime
